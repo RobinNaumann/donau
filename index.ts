a623bc7505d0941af4d20aab167a535542f04536
Robin Naumann <constorux@gmail.com>
2025-05-31 13:07:55 +0100

    feat: added frontend serving middleware

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ export * from "./src/auth/auth_jwt";
 export * from "./src/api/server";
 export * from "./src/models/m_api";
 export * from "./src/util/error";
-export { logger } from "./src/util/log";
+export * from "./src/util/log";
 export * from "./src/util/route";
 
 import * as express from "express";
--- servercalls/server.ts
+++ servercalls/server.ts
@@ -1,4 +1,14 @@
-import { err, grouped, route, routeAuthed, type DonauRoute } from "..";
+import path from "path";
+import {
+  chalk,
+  err,
+  express,
+  grouped,
+  logger,
+  route,
+  routeAuthed,
+  type DonauRoute,
+} from "..";
 import { type ServerCall } from "./shared";
 
 /**
@@ -60,4 +70,49 @@ export function handleServerCalls<
   });
 
   return grouped(routes, { tags: ["server-calls"], prefix: "/calls" });
+}
+
+/**
+ * a middleware that serves a SPA frontend (such as a react app) via donau at "/".
+ * @param clientPath the path to the client build folder. This should be the folder that contains the `index.html` file.
+ * Provide an absolute path or a relative path from the current working directory.
+ * @return a function that can be used to add the middleware to an express app or
+ * pass it as a `preService` to the donauServerRun function
+ */
+export function serveFrontend(
+  clientPath: string
+): (app: express.Express) => void {
+  return (app: express.Express) => {
+    const _clientPath = path.isAbsolute(clientPath)
+      ? path.normalize(clientPath)
+      : path.join(process.cwd(), clientPath);
+    // adapted from: https://leejjon.medium.com/33705be3ceda
+    // This code makes sure that any request that does not matches a static file
+    // in the build folder, will just serve index.html. Client side routing is
+    // going to make sure that the correct content will be loaded.
+    app.use((req, res, next) => {
+      if (
+        req.path === "/api" ||
+        req.path === "/docs" ||
+        req.path.startsWith("/api/") ||
+        req.path.startsWith("/docs/") ||
+        /(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)
+      ) {
+        next();
+      } else {
+        res.header(
+          "Cache-Control",
+          "private, must-revalidate, no-cache, no-store"
+        );
+        res.header("Expires", "-1");
+        res.header("Pragma", "no-cache");
+        res.sendFile(path.join(_clientPath, "index.html"));
+      }
+    });
+    app.use(express.static(_clientPath));
+
+    app.use("/app", express.static(_clientPath));
+
+    logger.success(`serving ${chalk.bold("frontend")} at: /`);
+  };
 }
